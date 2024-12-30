9a200c388b41684ffc229c27bb735f8664748d12
Szilard Nemeth <szilard.nemeth88@gmail.com>
2024-12-30 16:21:59 -0500

    Add: tampermonkey-trello-reoder-labels.js

---
+++ trello_com/tampermonkey-trello-reoder-labels.js
@@ -0,0 +1,9 @@
+function getShowMoreLabelsButtonJQuery() {
+    var button = $("button:contains(Show more labels)")[0]
+    return button
+}
+
+
+button = getShowMoreLabelsButtonJQuery()
+console.log(button)
+
